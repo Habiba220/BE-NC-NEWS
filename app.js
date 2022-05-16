c9143998f49ff81cda4ba460677a2a6b0088ed45
Habiba220 <habiba22mahran@gmail.com>
2022-05-16 14:51:34 +0100

    #3: GET /api/topics tests and logic

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { getTopics } = require("./controller/topics.controller");
+
+const app = express();
+
+app.get('/api/topics', getTopics)
+
+app.use("/*", (req, res, next) => {
+    res.status(404).send({ message: "Invalid endpoint" });
+  });
+  
+app.use((err, req, res, next) => {
+    if (err.status) {
+      res.status(err.status).send({ message: err.message });
+    } else {
+      next(err);
+    }
+  });
+  
+  app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send({ message: "Internal server error" });
+  });
+  
+  module.exports = app;
