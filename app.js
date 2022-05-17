55ffb72f7e0f0d8ea9aaac17501a5797fc83eee5
Habiba220 <habiba22mahran@gmail.com>
2022-05-17 15:38:57 +0100

    5 PATCH /api/articles/:article_id tested for happy and sad paths

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const { getArticleByID } = require("./controller/articles.controllers");
+const { getArticleByID, patchArticleByID } = require("./controller/articles.controllers");
 const { invalidEnpointError, customError, internalServerError, PSQLerror } = require("./controller/errors.controllers");
 const { getTopics } = require("./controller/topics.controller");
 
 const app = express();
+app.use(express.json())
 
 app.get('/api/topics', getTopics)
 app.get('/api/articles/:article_id', getArticleByID)
 
-app.use("/*", invalidEnpointError );
+app.patch('/api/articles/:article_id', patchArticleByID)
 
+app.use("/*", invalidEnpointError );
 app.use(PSQLerror)
-  
 app.use(customError);
-  
 app.use(internalServerError);
-  
+
+
   module.exports = app;
